7cc85bfe01fabff1d8e3db2703cd3de242d219aa
Kia <pruittzakiya@gmail.com>
2022-10-14 04:04:46 +0000

    adding to more recipes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Javascript for TP6
+// Javascript for TP7
 //function to load file from the URL "fromFile" into the object identified by "whereTo"
 function loadFileInto(fromFile, whereTo) {
 
@@ -11,7 +11,7 @@ function loadFileInto(fromFile, whereTo) {
 	// provides code to do something in response to the AJAX request
 	ajax.onreadystatechange = function() {
 			if ((this.readyState == 4) && (this.status == 200)) {
-				document.querySelector(whereTo).innerHTML += this.responseText;
+				document.querySelector(whereTo).innerHTML = this.responseText;
 				
 			} else if ((this.readyState == 4) && (this.status != 200)) {
 				console.log("Error: " + this.responseText);
@@ -29,12 +29,41 @@ function loadFileInto(fromFile, whereTo) {
 
 
 
+// new Recipe object  
+function Recipe(recipeName, contributorName, imageURL, ingredientsURL, equipmentURL, directionsURL) {
+  
+  this.recipeName = recipeName;
+  this.contributor = contributorName;
+  this.imageURL = imageURL;
+  this.ingredients =  ingredientsURL;
+  this.equipment = equipmentURL;
+  this.directions = directionsURL;
+  
+  this.displayRecipe = function() {
+    
+    document.querySelector("#container h1").innerHTML = this.recipeName; 
+    document.querySelector("contributor").innerHTML = this.contributor; 
+    document.querySelector(".centered").innerHTML = this.centered; "img(" + this.imageURL + ")";
+    LoadFileInto(this.ingredients, "#ingedients ul");
+    LoadFileInto(this.equipment, "#equipment  ul");
+    LoadFileInto(this.directions, "#directions ol");
+    
+  }
+ 
+  
+  
+}
 
+BasicRecipe = new Recipe( "Basic Recipe", "Kia", "https://images.unsplash.com/photo-1485962307416-993e145b0d0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", "ingredients.html", "equipment.html", "directions.html");
 
 
 window.onload = function() {
   
-    
+    document.querySelector("#fristRecipe").onclick = function() {
+      SevenLayerBars.displayRecipe();
+      
+      
+    }
 
   
 
